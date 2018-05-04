0f0c1a392dea89ac2e28a6e627407afe52daa8d3
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-05-04 19:19:38 +0700

    chuc nang login va signup kết nối database thông qua webservice

---
+++ FoodNtest/app/src/main/java/com/example/admin/model/User.java
@@ -0,0 +1,35 @@
+package com.example.admin.model;
+
+/**
+ * Created by Windows 10 on 5/3/2018.
+ */
+
+public class User {
+    private String hoten;
+    private String sdt;
+    private String matkhau;
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+}
