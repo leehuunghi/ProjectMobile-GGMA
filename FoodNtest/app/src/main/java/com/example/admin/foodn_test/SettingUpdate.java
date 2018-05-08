e64d319c486ead579c46e1b4c06009b717c88c41
donguyen131197 <do.nguyen131197@gmail.com>
2018-05-08 09:03:34 +0700

    Up tiếp 1 cái gì đó

--- FoodNtest/app/src/main/java/com/example/admin/foodn_test/SettingUpdate.java
+++ FoodNtest/app/src/main/java/com/example/admin/foodn_test/SettingUpdate.java
@@ -1,15 +1,24 @@
 package com.example.admin.foodn_test;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SettingUpdate extends AppCompatActivity {
 
-    EditText txtUBanKinh, txtUCheDoXem;
     Button btnCapNhatSetting;
+    LinearLayout sugRad;
+    LinearLayout viewMode;
+    TextView sugRadResult;
+    TextView viewModeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,21 @@ public class SettingUpdate extends AppCompatActivity {
     }
 
     private void addEvents() {
+
+        sugRad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogRadius();
+            }
+        });
+
+        viewMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogViewMode();
+            }
+        });
+
         btnCapNhatSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,10 +53,85 @@ public class SettingUpdate extends AppCompatActivity {
         });
     }
 
-    private void addControls() {
-        txtUBanKinh=findViewById(R.id.txtUBanKinh);
-        txtUCheDoXem=findViewById(R.id.txtUCheDoXem);
+    public void showDialogRadius() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingUpdate.this);
+        builder.setTitle(R.string.suggested_radius);
 
+        //list of items
+        final String[] items = getResources().getStringArray(R.array.radius);
+        builder.setSingleChoiceItems(items, 0,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        String input = items[which].toString();
+//                        sugRadResult.setText(input);
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // negative button logic
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
+    }
+
+    public void showDialogViewMode() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingUpdate.this);
+        builder.setTitle(R.string.view_mode);
+
+        //list of items
+        final String[] items = getResources().getStringArray(R.array.view_mode);
+        builder.setSingleChoiceItems(items, 0,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        String input = items[which].toString();
+//                        sugRadResult.setText(input);
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // negative button logic
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
+    }
+
+    private void addControls() {
+        sugRad = findViewById(R.id.sugRad);
+        viewMode = findViewById(R.id.viewMode);
+        sugRadResult = findViewById(R.id.sugRadResult);
+        viewModeResult = findViewById(R.id.viewModeResult);
         btnCapNhatSetting=findViewById(R.id.btnCapNhatSetting);
     }
 }
