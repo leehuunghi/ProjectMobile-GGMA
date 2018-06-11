d6521fdccefed27e70494efb428ddd64ad5539ca
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-06-11 12:56:50 +0700

    Thêm hình từ điện thoại lên

--- FoodNAdmin/app/src/main/java/com/example/admin/foodn_test/AddingStore.java
+++ FoodNAdmin/app/src/main/java/com/example/admin/foodn_test/AddingStore.java
@@ -1,21 +1,36 @@
 package com.example.admin.foodn_test;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.admin.foodn_test.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AddingStore extends AppCompatActivity {
 
     EditText txtTenCuaHang, txtDiaChiCuaHang, txtSDTCuaHang, txtMoCua, txtDongCua;
     Button btnQuanLyMenu;
-    GridView gridView;
-    ImageView imgLocationCuaHang;
+    ImageView imgImageStore;
+    ImageButton btnAdd, btnEditStore;
+
+    private static int LOAD_IMAGE_RESULTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,8 @@ public class AddingStore extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
         getSupportActionBar().setTitle("Thêm quán");
+
+
     }
 
     @Override
@@ -50,13 +67,56 @@ public class AddingStore extends AppCompatActivity {
             }
         });
 
-        imgLocationCuaHang.setOnClickListener(new View.OnClickListener() {
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intMap=new Intent(AddingStore.this,HomeActivity.class);
-                startActivity(intMap);
+                // Create the Intent for Image Gallery.
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+               // i.setType("image/*");
+                // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+                startActivityForResult(i, LOAD_IMAGE_RESULTS);
             }
         });
+
+        btnEditStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create the Intent for Image Gallery.
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                // i.setType("image/*");
+                // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+                startActivityForResult(i, LOAD_IMAGE_RESULTS);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultcode, Intent intent) {
+        super.onActivityResult(requestCode, resultcode, intent);
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == LOAD_IMAGE_RESULTS && resultcode == RESULT_OK && intent != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = intent.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            // Now we need to set the GUI ImageView data with data read from the picked file.
+            imgImageStore.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+
+            btnAdd.setVisibility(View.INVISIBLE);
+            btnEditStore.setVisibility(View.VISIBLE);
+
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+        }
     }
 
     private void addControls() {
@@ -67,8 +127,11 @@ public class AddingStore extends AppCompatActivity {
         txtDongCua=findViewById(R.id.txtDongCua);
 
         btnQuanLyMenu=findViewById(R.id.btnQuanLyMenu);
+        btnAdd=findViewById(R.id.btnAdd);
+        imgImageStore=findViewById(R.id.imgImageStore);
+        btnEditStore=findViewById(R.id.btnEditStore);
 
-        gridView=findViewById(R.id.gridView);
-        imgLocationCuaHang=findViewById(R.id.imgLocationCuaHang);
+        btnEditStore.setVisibility(View.INVISIBLE);
     }
+
 }
